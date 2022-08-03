282b8a6e9047f8186d583c0176e4a49782769cbc
Aliaksandr Hapankou <akkforrs@gmail.com>
2022-08-03 21:25:52 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+    const num = Math.abs(n)
+
+    return (
+    parseFloat(
+      num
+        .toString()
+        .split('')
+        .reverse()
+        .join('')
+    ) 
+  )
 }
